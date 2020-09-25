de2e1ee6526494ad6ab1f20ee2656d0624c2324e
Devenda <tinus_scheppers@telenet.be>
2020-09-25 12:24:13 +0200

    Add frontend error handling

---
+++ DeskPlan/Components/IErrorComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeskPlan.Components
+{    public interface IErrorComponent
+    {
+        void ShowError(string title, string message);
+    }
+}
